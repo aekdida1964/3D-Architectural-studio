f61d7d5441b0e73e747f5f817e7a08c0aafdf33f
aekdida1964 <aekdida1964@gmail.com>
2026-09-04 04:16:20 +0100

    Add files via upload

--- Src/UI/UImanager.js
+++ Src/UI/UImanager.js
@@ -1,1 +1,20 @@
+export class UIManager {
+  constructor() {
+    this.loader = document.getElementById('loader');
+    this.loaderText = document.getElementById('loader-text');
+    this.measureResult = document.getElementById('measure-result');
+  }
 
+  showLoader(text = 'جاري التحميل...') {
+    this.loaderText.innerText = text;
+    this.loader.classList.remove('hidden');
+  }
+
+  hideLoader() {
+    this.loader.classList.add('hidden');
+  }
+
+  updateMeasureResult(distance) {
+    this.measureResult.innerText = `المسافة: ${distance} متر`;
+  }
+}
